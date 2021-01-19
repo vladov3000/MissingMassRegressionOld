50e8465ee62f24ca4f821d5212f20c7531d1a2eb
vlad <vladov3000@gmail.com>
2021-01-19 13:31:49 -0800

    results

--- scripts/wlnu_dataset.py
+++ scripts/wlnu_dataset.py
@@ -4,5 +4,9 @@ import torch
 class WLNuDataset(torch.utils.data.Dataset):
     def __init__(self, df_path="data/gen.csv"):
         self.df = pd.read_csv(df_path)
-        print(self.df)
 
+    def __getitem__(self):
+        pass
+
+    def __len__(self):
+        pass
