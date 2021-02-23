0a293be89c3f81caaf55954718820472edcdf939
vlad <vladov3000@gmail.com>
2021-02-23 15:12:51 -0800

    added scaling

--- hwwlnulnu/dataset.py
+++ hwwlnulnu/dataset.py
@@ -3,12 +3,18 @@ from argparse import ArgumentParser
 import pandas as pd
 import pytorch_lightning as pl
 import torch
-from sklearn import preprocessing
 from torch.utils.data import DataLoader
+import numpy as np
+
+import os
+import pickle
 
 
 class HWWDataset(torch.utils.data.Dataset):
-    def __init__(self, loss_type="nu", df_path="data/h_ww_lnulnu.pkl"):
+    def __init__(self,
+                 loss_type="nu",
+                 df_path="data/h_ww_lnulnu.pkl",
+                 ave_std_path="data/ave_std.pkl"):
 
         Na = [
             "Na_Genx",
@@ -98,6 +104,27 @@ class HWWDataset(torch.utils.data.Dataset):
         df = pd.read_pickle(df_path)
         df = df[self.inputs + self.targets]
 
+        # stores average, standard deviation for every column
+        stats = {}
+        if os.path.exists(ave_std_path):
+            with open(ave_std_path, "rb") as f:
+                stats = pickle.load(f)
+        else:
+            stats = {}
+
+            for col in df.columns:
+                stats[f"ave_{col}"] = df[col].mean()
+                stats[f"std_{col}"] = df[col].std()
+
+            with open(ave_std_path, "wb+") as f:
+                pickle.dump(stats, f)
+
+        # standardize columns
+        for col in df.columns:
+            ave = stats[f"ave_{col}"]
+            std = stats[f"std_{col}"]
+            df[col] = (df[col] - ave) / (std + 1e-8)
+
         self.data = torch.from_numpy(df.values).float()
 
     def __getitem__(self, idx):
@@ -120,6 +147,7 @@ class HWWDataModule(pl.LightningDataModule):
 
         super().__init__(*args, **kwargs)
         self.df_path = f"{data_path}/h_ww_lnulnu.pkl"
+        self.ave_std_path = f"{data_path}/ave_std.pkl"
         self.data_split = data_split
         self.batch_size = batch_size
         self.num_workers = num_workers
@@ -128,7 +156,9 @@ class HWWDataModule(pl.LightningDataModule):
         self.setup()
 
     def setup(self):
-        dataset = HWWDataset(df_path=self.df_path, loss_type=self.loss_type)
+        dataset = HWWDataset(df_path=self.df_path,
+                             ave_std_path=self.ave_std_path,
+                             loss_type=self.loss_type)
 
         data_split = [int(i * len(dataset)) for i in self.data_split]
 
--- hwwlnulnu/model.py
+++ hwwlnulnu/model.py
@@ -83,11 +83,11 @@ class HWWModel(pl.LightningModule):
         self.log("train_loss", loss)
 
     def validation_epoch_end(self, outputs):
-        loss = torch.stack([x for x in outputs]).mean()
+        loss = torch.stack(outputs).mean()
         self.log("val_loss", loss)
 
     def test_epoch_end(self, outputs):
-        loss = torch.stack([x for x in outputs]).mean()
+        loss = torch.stack(outputs).mean()
         self.log("test_loss", loss)
 
     @staticmethod
